58ad863d107510c83112795bf817921c56a70550
DeD MorozZz <morozzz.g2@gmail.com>
2016-02-26 18:28:59 +0300

    Initial commit

---
+++ main.v
@@ -0,0 +1,63 @@
+`include "ram.v"
+
+`timescale 1 ns / 10 ps
+
+module main();
+
+    reg [31:0] w_addr;
+    reg [31:0] r_addr;
+    reg [31:0] w_line;
+    reg clk;
+    reg read;
+    reg write;
+
+    wire [31:0] r_line1;
+    wire [31:0] r_line2;
+    wire exc1;
+    wire exc2;
+    wire wrdy, rrdy;
+
+    ram R1(r_addr, w_addr, r_line1, w_line, read, write, wrdy, rrdy, exc1, clk);
+    emb_ram R2(r_addr, w_addr, r_line2, w_line, read, write, exc2, clk);
+
+    integer i;
+    time t1, t2;
+
+    initial begin
+        w_addr = 32'b0;
+        r_addr = 32'b0;
+        w_line = 32'b0;
+        clk = 1'b1;
+        read = 1'b0;
+        write = 1'b0;
+
+        $dumpfile("dump.fst");
+        $dumpvars(0);
+        $dumpon;
+    end
+
+    always begin //clock generator
+        #1 clk = 0;
+        #1 clk = 1;
+    end
+
+    always begin //testcase
+        for(i = 0; i < 1024; i++) begin
+            w_line <= i << 1;
+            w_addr <= i + 1;
+            write <= 1;
+            r_addr <= i;
+            read <= 1;
+            wait(!wrdy && !rrdy);
+            wait(wrdy && rrdy);
+            write <= 0;
+            $display(r_line1);
+            $display(r_line2);
+            read <= 0;
+            #0;
+        end
+        $dumpflush;
+        $finish;
+    end
+
+endmodule
---
+++ ram.v
@@ -0,0 +1,113 @@
+
+`timescale 1 ns / 10 ps
+
+module ram(r_addr, w_addr, r_line, w_line, read, write, wrdy, rrdy, exc, clk);
+    input [31:0] r_addr;
+    input [31:0] w_addr;
+    input [31:0] w_line;
+    input        read;
+    input        write;
+    input        clk;
+
+    output [31:0] r_line;
+    reg    [31:0] r_line;
+    output        exc;
+    reg           exc;
+    output        wrdy, rrdy;
+    reg           wrdy, rrdy;
+
+    //memory
+    parameter mem_size = 262144; //1mb, 4b/w
+
+    reg [31:0] mem [mem_size:0];
+
+    integer i;
+
+    initial begin
+        for(i = 0; i < mem_size; i++) begin
+            mem[i] = 32'b0;
+        end
+        r_line = 32'b0;
+        exc = 1'b0;
+        wrdy = 1'b0;
+        rrdy = 1'b0;
+    end
+
+    always @(posedge clk) begin
+        if(wrdy) wrdy <= 1'b0;
+        if(rrdy) rrdy <= 1'b0;
+
+        if(read & !rrdy ) begin
+            if(r_addr >= mem_size) begin
+                r_line <= 32'b0;
+                exc <= 1'b1;
+            end
+            else begin
+                r_line <= mem[r_addr];
+                rrdy <= 1'b1;
+                exc <= 1'b0;
+            end
+        end
+        else r_line = 32'bz;
+
+        if(write && !wrdy) begin
+            if(w_addr >= mem_size) exc <= 1'b1;
+            else begin
+                mem[w_addr] <= w_line;
+                wrdy <= 1'b1;
+                exc <= 1'b0;
+            end
+        end
+    end
+endmodule
+
+module emb_ram(r_addr, w_addr, r_line, w_line, read, write, exc, clk);
+    input [31:0] r_addr;
+    input [31:0] w_addr;
+    input [31:0] w_line;
+    input        read;
+    input        write;
+    input        clk;
+
+    output [31:0] r_line;
+    reg    [31:0] r_line;
+    output        exc;
+    reg           exc;
+
+    //memory
+    parameter mem_size = 262144; //1mb, 4b/w
+
+    reg [31:0] mem [mem_size:0];
+
+    integer i;
+
+    initial begin
+        for(i = 0; i < mem_size; i++) begin
+            mem[i] = 32'b0;
+        end
+        r_line = 32'b0;
+        exc = 1'b0;
+    end
+
+     always @(posedge clk) begin
+        if(read) begin
+            if(r_addr >= mem_size) begin
+                r_line <= 32'b0;
+                exc <= 1'b1;
+            end
+            else begin
+                r_line <= mem[r_addr];
+                exc <= 1'b0;
+            end
+        end
+        else r_line <= 32'bz;
+
+        if(write) begin
+            if(w_addr >= mem_size) exc <= 1'b1;
+            else begin
+                mem[w_addr] <= w_line;
+                exc <= 1'b0;
+            end
+        end
+    end
+endmodule
